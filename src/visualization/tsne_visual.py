1d64b5ac875e52ff023b4121b56be152b1666155
gaunby <s174497@student.dtu.dk>
2022-11-10 15:22:04 +0100

    all visual + single plot

--- src/visualization/tsne_visual.py
+++ src/visualization/tsne_visual.py
@@ -49,24 +49,34 @@ def visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualiz
 
 
 
-def visualize_one_layer(hidden_state,labels,title, perplexity, init, save = False):
+def visualize_one_layer(hidden_state,labels,title, perplexity, init, layer_name, save = False):
     dim_reducer = TSNE(n_components=2, init = init, perplexity = perplexity)
     
-    plt.figure(figsize=(10,6)) #each subplot of size 6x6, each row will hold 4 plots
-    plt.title(f't-SNE of embedding in RoBERTa')
+    n = len(np.array(labels).reshape(-1))
+    label_text = np.array(['nan']*n)
+    label_text = np.where(np.array(labels).reshape(-1) == 0, label_text, 'neg')
+    label_text = np.where(np.array(labels).reshape(-1) == 1, label_text, 'pos')
+
+
+    plt.figure(figsize=(10,6)) # 
+    plt.title(f't-SNE of {layer_name} in RoBERTa', fontsize =20)
 
     labels = np.array(labels).reshape(-1)
     
     layer_averaged_hidden_states = hidden_state.mean(axis=1)
     layer_dim_reduced_embeds = dim_reducer.fit_transform(layer_averaged_hidden_states);
     print('>>> t-SNE output dim:',layer_dim_reduced_embeds.shape)
-    df = pd.DataFrame.from_dict({'x':layer_dim_reduced_embeds[:,0],'y':layer_dim_reduced_embeds[:,1],'label':labels})
-    
-    scatter = sns.scatterplot(data=df,x='x',y='y',hue='label');
-    sns.move_legend( scatter, loc = "upper left", fontsize = 'x-large');
-    scatter.set_xlabel(fontsize = 'xx-large');
-    scatter.set_ylabel(fontsize =  'xx-large');
+    df = pd.DataFrame.from_dict({'first dim':layer_dim_reduced_embeds[:,0],'second dim':layer_dim_reduced_embeds[:,1],'label':labels, 'label_text': label_text})
+
+    #df = pd.DataFrame.from_dict({'x':layer_dim_reduced_embeds[:,0],'y':layer_dim_reduced_embeds[:,1],'label':labels})
+    palette ={0:sns.color_palette("Paired")[0], 1:sns.color_palette("Paired")[6],
+                'neg':sns.color_palette("Paired")[1], 'pos':sns.color_palette("Paired")[7]}
+    scatter = sns.scatterplot(data=df,x='first dim',y='second dim',hue='label_text', palette = palette );
+    scatter.legend(loc = 'upper right',fontsize = 'x-large', title = 'class',title_fontsize='x-large');
+    scatter.set_xlabel('1st dim',fontsize=16);
+    scatter.set_ylabel('2nd dim',fontsize=16);
+    scatter.tick_params(labelsize='x-large');
 
     if save:  
         print('>>> save fig. <<<')  
-        plt.savefig(f'/zhome/94/5/127021/speciale/master_project/src/visualization/figures/hidden_layers_{title}_{perplexity}_{init}',format='png',pad_inches=0)
+        plt.savefig(f'/zhome/94/5/127021/speciale/master_project/src/visualization/figures/{layer_name}_{title}_{perplexity}_{init}.pdf',format='pdf',pad_inches=0)
--- src/visualization/tsne_visual_save.py
+++ src/visualization/tsne_visual_save.py
@@ -2,7 +2,7 @@
 import pickle
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
 from datasets import load_from_disk
-from tsne_visual import visualize_layerwise_embeddings
+from tsne_visual import visualize_layerwise_embeddings, visualize_one_layer
 
 # load data
 #datadir = '/work3/s174498/sst2_dataset/'
@@ -32,12 +32,15 @@ with open('/work3/s174498/roberta_files/test_output_roberta_head_linear.pickle',
     output_linear = pickle.load(handle)
 
 with open('/work3/s174498/roberta_files/test_logits_dense_output_roberta_head_nn.pickle', 'rb') as handle:
+    output_nn = pickle.load(handle)
+
+with open('/work3/s174498/roberta_files/output_roberta_base.pickle', 'rb') as handle:
     output_original = pickle.load(handle)
+
 print('>>> create fig <<<')
 
-# global 
+# fine tune linear 
 labels = output_linear.label_ids
-
 hidden_states = output_linear.predictions[1][1:]
 title = 'linear_results'
 layers_to_visualize = [0,1,2,3,4,5,6,7,8,9,10,11]
@@ -49,16 +52,54 @@ visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, p
 perplexity = 50
 visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init, save = True)
 
+# linear-head
+sequence_output = output_linear.predictions[2]
+layer_name = 'encoder-output'
+perplexity = 10
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 30
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 50
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
 
+
+# finetune NN
+labels = output_nn.label_ids
+hidden_states = output_nn.predictions[1][1:]
+title = 'nn_results'
+layers_to_visualize = [0,1,2,3,4,5,6,7,8,9,10,11]
+init = 'pca'
+perplexity = 10
+visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init, save = True)
+perplexity = 30
+visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init, save = True)
+perplexity = 50
+visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init, save = True)
+# nn-head
+sequence_output = output_nn.predictions[2]
+layer_name = 'encoder-output'
+perplexity = 10
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 30
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 50
+visualize_one_layer(sequence_output,labels,title, perplexity, init, layer_name, save = True)
+# from original proposed RobertaSequenceHead
+logits_dense = output_nn.predictions[3]
+layer_name = 'head-dense'
+perplexity = 10
+visualize_one_layer(logits_dense,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 30
+visualize_one_layer(logits_dense,labels,title, perplexity, init, layer_name, save = True)
+perplexity = 50
+visualize_one_layer(logits_dense,labels,title, perplexity, init, layer_name, save = True)
+
+# base model 
 labels = output_original.label_ids
-
 hidden_states = output_original.predictions[1][1:]
 title = 'original_results'
 layers_to_visualize = [0,1,2,3,4,5,6,7,8,9,10,11]
 init = 'pca'
-#perplexity = 10
-#visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init)
 perplexity = 30
 visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init, save = True)
-#perplexity = 50
-#visualize_layerwise_embeddings(hidden_states,labels,title,layers_to_visualize, perplexity,init)
+
