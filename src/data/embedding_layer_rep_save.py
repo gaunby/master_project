3316c51803bd875a21bc67bb22461767842f5b09
gaunby <s174497@student.dtu.dk>
2022-11-25 15:57:38 +0100

    computing correct cavs

--- src/data/embedding_layer_rep_save.py
+++ src/data/embedding_layer_rep_save.py
@@ -13,10 +13,33 @@ random_text = random_data['complex_sentence']
 
 data = random_text
 classifier = 'linear'
+num_random_set = 500
+num_ex_in_set = 300
+
+# layer 11
 model_layer = "roberta.encoder.layer.11.output.dense"
 model_layer_num = '11'
-num_random_set = 500
-num_ex_in_set = 150
+
+random_rep = create_embedding(random_text, classifier, model_layer, num_random_set= num_random_set, num_ex_in_set= num_ex_in_set )
+
+name = f'tensor_{Data}_on_{model_layer_num}_layer_{num_random_set}_sets_with_{num_ex_in_set}'
+file = PATH_TO_Data + Data + '/' + name + '.pt'
+torch.save(random_rep, file)
+
+# layer 10
+model_layer = "roberta.encoder.layer.10.output.dense"
+model_layer_num = '10'
+
+random_rep = create_embedding(random_text, classifier, model_layer, num_random_set= num_random_set, num_ex_in_set= num_ex_in_set )
+
+name = f'tensor_{Data}_on_{model_layer_num}_layer_{num_random_set}_sets_with_{num_ex_in_set}'
+file = PATH_TO_Data + Data + '/' + name + '.pt'
+torch.save(random_rep, file)
+
+# layer 2
+model_layer = "roberta.encoder.layer.2.output.dense"
+model_layer_num = '2'
+
 random_rep = create_embedding(random_text, classifier, model_layer, num_random_set= num_random_set, num_ex_in_set= num_ex_in_set )
 
 name = f'tensor_{Data}_on_{model_layer_num}_layer_{num_random_set}_sets_with_{num_ex_in_set}'
--- src/models/hate_tcav/TCAV.py
+++ src/models/hate_tcav/TCAV.py
@@ -10,6 +10,9 @@ from src.models.hate_tcav.Roberta_model_data import RobertaClassifier,ToxicityDa
 import random
 from datasets import load_from_disk
 import time
+from sklearn import linear_model, metrics
+from sklearn.model_selection import train_test_split
+
 
 #random.seed(100)
 
@@ -39,6 +42,7 @@ model_folder_CH_explicit = PATH_TO_Model+'explicit_CH_roberta'
 model_folder_toxic_explicit = PATH_TO_Model+'explicit_wiki_roberta'
 """
 
+
 def get_dataloader(X, y, tokenizer, batch_size):
   assert len(X) == len(y)
   encodings = tokenizer(X, truncation=True, padding=True, return_tensors="pt")
@@ -66,27 +70,78 @@ def get_reps(model,tokenizer, concept_examples):
   
   return concept_repres
 
-def compute_cavs(model, tokenizer, concept_text, random_rep, num_runs=10):
+def train_lm(lm, x, y, labels2text):
+  x_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.33, stratify=y
+        )
+  lm.fit(x_train, y_train)
+  y_pred = lm.predict(x_test)
+
+  # get acc.
+  num_classes = max(y) + 1
+  acc = {}
+  num_correct = 0
+  for class_id in range(num_classes):
+      # get indices of all test data that has this class.
+      idx = y_test == class_id
+      acc[labels2text[class_id]] = metrics.accuracy_score(
+          y_pred[idx], y_test[idx]
+      )
+      # overall correctness is weighted by the number of examples in this class.
+      num_correct += sum(idx) * acc[labels2text[class_id]]
+  acc["overall"] = float(num_correct) / float(len(y_test))
+
+  return lm, acc
+
+
+def compute_cavs(model, tokenizer, concept_text, random_rep, num_runs=500):
   #calculates CAVs
+  # num_runs: should be num_random_set
   cavs = []
-  
-  concept_rep = get_reps(model,tokenizer,concept_text)
-  
+  acc = []
+  class_concept = get_reps(model,tokenizer,concept_text)
+  N = len(class_concept)
+  if len(random_rep) != num_runs*N:
+    print('Incorrect number of random samples.\nNeed',num_runs*N, 'but have',len(random_rep))
+    return
   #####
   #####
   # build from here 
-
-
-
-  #####
-  #####
-  for i in range(num_runs):
-    concept_rep_ids = list(np.random.choice(range(len(concept_repres)), 30))
-    concept_rep = [concept_repres[i] for i in concept_rep_ids]
-    cavs.append(np.mean(concept_rep, axis = 0))
+  lm = linear_model.SGDClassifier(
+      alpha=0.01,
+      max_iter=5000,
+      tol=1e-3,
+  )
+  labels2text = {}
+  labels2text[0] = 'concept'
+  labels2text[1] = 'random'
+  for i in range(2): # range(num_runs):
+    class_random = random_rep[i*N:(i+1)*N]
+    x = []
+    labels = []
+    x.extend(class_concept)
+    labels.extend([0]*N)
+    x.extend(class_random)
+    labels.extend([1]*N)
     
+    x = np.array(x)
+    labels = np.array(labels)
+
+    lm, acc_ = train_lm(lm, x, labels, labels2text)
+
+    #print('>>> Train LM coef:',len(lm.coef_))
+    #print(len(lm.coef_[0]))
+    
+    # what they do in TCAV Been Kim : 
+    # cavs.extend([-1 * lm.coef_[0],lm.coef_[0]])
+    cavs.extend([lm.coef_[0]])
+    acc.extend(acc_)
+    #print(cavs)
+    #print(len(cavs))
+    
+  # cavs[4] is cav-run number 4
   # cavs: list of arrays (the arrays are a list)
-  return cavs
+  return cavs, acc
 
 # old function
 def statistical_testing(model, tokenizer, concept_examples, num_runs=10): # old function
@@ -105,32 +160,19 @@ def get_logits_grad(model, tokenizer, sample, desired_class):
   model.zero_grad()
   input_ids = input['input_ids'].to(device) # idx from vocab
   
-  #print('input', len(input))
-  #print('>>> GET LOGITS GRAD input ids', input_ids)
-  
   attention_mask = input['attention_mask'].to(device)
-  logits, _, representation = model(input_ids, attention_mask=attention_mask)
-  
-  #print('logits',logits) # tror rigtig meget at logits er output 
-  #print('rep', representation.shape)
+  logits, _, _ = model(input_ids, attention_mask=attention_mask)
   
   logits[0, desired_class].backward()
-  #print('logits',logits)
-  
-  #print('cav shape',cav.shape)
   grad = model.grad_representation # differs with input sample
-
-  #print('grad 1:', grad[0:3,0:3])
-  #print('grad 2:', grad.shape)
   
-  #print('first',grad.shape)
   grad = grad[0][0].cpu().numpy()#grad.sum(dim=0).squeeze().cpu().detach().numpy()
   return logits,grad
 
 #def get_preds_tcavs(classifier = 'toxicity',desired_class = 1,examples_set = 'random',concept_examples = random_concepts, num_runs = 10):
-def get_preds_tcavs(classifier = 'linear',model_layer = "roberta.encoder.layer.11.output.dense",desired_class = 1,counter_set = 'wikipedia_split',concept_text = random_text, num_runs = 10):
+def get_preds_tcavs(classifier = 'linear',model_layer = "roberta.encoder.layer.11.output.dense", layer_nr = '11',target_text = random_text,desired_class = 0,counter_set = 'wikipedia_split',concept_text = random_text, num_runs = 10):
   #returns logits, sensitivies and tcav score
-  num_random_set = 10
+  num_random_set = num_runs #num_random_set
   # load tokenizer 
   if classifier == 'linear':
     folder = '/work3/s174498/final/linear_head/checkpoint-1500'
@@ -174,64 +216,65 @@ def get_preds_tcavs(classifier = 'linear',model_layer = "roberta.encoder.layer.1
     return
 
   if counter_set=='wikipedia_split':
+    
     num_ex_in_set = len(concept_text)
-    Data = 'wikipedia_split'
-    file_name = f'tensor_{Data}_on_{model_layer}_{num_random_set}_sets_with_{num_ex_in_set}'
+    Data = counter_set #'wikipedia_split'
+    file_name = f'tensor_{Data}_on_{layer_nr}_layer_{num_random_set}_sets_with_{num_ex_in_set}'
     file_random = PATH_TO_Data + '/'+ Data + '/' + file_name + '.pt'
-    random_rep = torch.load(file_random)
-    #print('Number of random examples:', len(random_text))#
-    #random_examples = [random_text[i][5:-1] for i in list(np.random.choice(len(random_text),N*num_random_set))]
+
+    if os.path.exists(file_random):
+      random_rep = torch.load(file_random)
+    else:
+      print('Counter part does not have a representation for this model layer or does not have the correct size.\nCreate by running: embedding_layer_rep.py')
+      return
+
   else:
-    print('Counter part does not have a representation for this layer\nCreate by running: embedding_layer_rep.py')
+    print('Counter part does not have a representation for this random dataset\nCreate by running: embedding_layer_rep.py')
     return
 
   print('calculating cavs...')
   model.to(device)
-  concept_cavs = compute_cavs(model,tokenizer, concept_text, random_rep, num_runs=num_runs)
+  concept_cavs, acc = compute_cavs(model,tokenizer, concept_text, random_rep, num_runs=num_runs)
+  
   #concept_cavs = statistical_testing(model,tokenizer, concept_examples, num_runs=num_runs)
-  save = False
-  """
-  if os.path.exists('grads_logits/'+classifier+'_'+examples_set+'_'+str(desired_class)+'.pkl'):
+  
+  if os.path.exists(PATH_TO_Data+'sst2_dataset/grads_logits/'+classifier+'_class_'+str(desired_class)+'_layer_'+layer_nr+'.pkl'):
     print('logits and grads are saved.')
-    with open('grads_logits/'+classifier+'_'+examples_set+'_'+str(desired_class)+'.pkl','rb') as handle:
+    with open(PATH_TO_Data+'sst2_dataset/grads_logits/'+classifier+'_class_'+str(desired_class)+'_layer_'+layer_nr+'.pkl','rb') as handle:
       data = pickle.load(handle)
-    
     grads = data['grads']
     logits = data['logits']
-    """
-  if save:
-    A = 0
   else:
     print('>>> calculating logits and grads...')
     logits = []
     grads = []
-    for sample in examples:
+    for sample in target_text:
       logit,grad = get_logits_grad(model, tokenizer, sample, desired_class)
-      #print('>>> logit', logit)
-      #print('>>> grad', grad.shape)
+
       grads.append(grad)
       logits.append(logit)
       data ={'grads':grads,
             'logits':logits}
-    print(os.getcwd())
-    with open('grads_logits/'+classifier+'_'+examples_set+'_'+str(desired_class)+'.pkl', 'wb') as handle:
+    print('>>> working dir:',os.getcwd())
+    with open(PATH_TO_Data+'sst2_dataset/grads_logits/'+classifier+'_class_'+str(desired_class)+'_layer_'+layer_nr+'.pkl', 'wb') as handle:
       pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
-   
   sensitivities = [] 
   print('>>> FOR LOOP GRADS', len(grads))
-  for grad in grads:
-    sensitivities.append([np.dot(grad, cav) for cav in concept_cavs])
-  sensitivities = np.array(sensitivities)
-  print('>>> sensitivet\n',sensitivities)
+  for cav in concept_cavs:
+    sensitivities.append([np.dot(grad, -1*cav) for grad in grads])
+
+  sensitivities = np.array(sensitivities) # each row is all grads on one cav 
+  
+  print('>>> sensitivet\n',sensitivities.shape)
   print('>>> concetp cavs', len(concept_cavs))
-  print(sensitivities.shape)
-  print('examples length', len(examples))
+  
   tcavs = []
+  print('>>> length of target text:', len(target_text))
   for i in range(num_runs):
-    tcavs.append(len([s for s in sensitivities[:,i] if s>0])/len(examples))
-   
+    tcavs.append(len([s for s in sensitivities[i,:] if s<0])/len(target_text))
+  
   print('TCAV score for the concept: ')
   print(np.mean(tcavs),np.std(tcavs)) 
   
-  return logits, sensitivities, tcavs
+  return logits, sensitivities, tcavs, acc
